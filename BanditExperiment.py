69ee645ec97a8ca9d7d88940e7b59b5782831a82
Max Lytovka <litmax515@gmail.com>
2024-02-21 16:45:30 +0100

    Implemented UCB policy evaluation (3c) - RAW & UNEDITED

--- BanditExperiment.py
+++ BanditExperiment.py
@@ -12,7 +12,7 @@ from BanditEnvironment import BanditEnvironment
 from BanditPolicies import EgreedyPolicy, OIPolicy, UCBPolicy
 from Helper import LearningCurvePlot, ComparisonPlot, smooth
 
-from tqdm import tqdm
+from tqdm import tqdm  # DELETE later
 
 def run_repetitions(
         n_actions: int,
@@ -26,17 +26,19 @@ def run_repetitions(
         average_reward = np.zeros(n_timesteps)
         cum_reward = 0
         bandit = BanditEnvironment(n_actions=n_actions)
-        if policy_type == EgreedyPolicy:
-            policy = policy_type(n_actions=n_actions)
-        else:
+        if policy_type == OIPolicy:
             policy = policy_type(
                 n_actions=n_actions,
                 initial_value=kwargs["initial_value"],
                 learning_rate=kwargs["learning_rate"]
             )
+        else:
+            policy = policy_type(n_actions=n_actions)
         for timestep in range(1, n_timesteps):
             if policy_type == EgreedyPolicy:
                 action_number = policy.select_action(epsilon=kwargs["epsilon"])
+            elif policy_type == UCBPolicy:
+                action_number = policy.select_action(c=kwargs["c"], t=timestep)
             else:
                 action_number = policy.select_action()
             reward = bandit.act(a=action_number)
@@ -84,6 +86,21 @@ def experiment(n_actions, n_timesteps, n_repetitions, smoothing_window):
 
     
     # Assignment 3: UCB
+    ucb_graph = LearningCurvePlot(title="UCB learning curves")
+    c_values = [0.01, 0.05, 0.1, 0.25, 0.5, 1.0]
+    for c_value in c_values:
+        curve = run_repetitions(
+            n_actions=n_actions,
+            n_timesteps=n_timesteps,
+            n_rep=n_repetitions,
+            policy_type=UCBPolicy,
+            c=c_value
+        )
+        ucb_graph.add_curve(
+            y=smooth(y=curve, window=smoothing_window),
+            label=f"c={c_value}"
+        )
+    ucb_graph.save(name="ucb_plot.png")
     
     # Assignment 4: Comparison
     
