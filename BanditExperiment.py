01ed95a293ef8edc2842eff8d8958589232184d7
Nataliia Bagan <natulko@Nataliias-MacBook-Air.local>
2024-02-26 21:33:40 +0100

    comments added p.s. Max dyakuyu za kod real mega tze roznos

--- BanditExperiment.py
+++ BanditExperiment.py
@@ -24,9 +24,11 @@ def run_repetitions(
 ):
     total_reward = np.zeros(num_timesteps)
     for _ in tqdm(range(n_rep)):
+        # Clear bandit environment
         bandit = BanditEnvironment(n_actions=num_actions)
         average_reward = np.zeros(num_timesteps)
         cum_reward = 0
+        # Initialise policy, declare correct arguments depending on policy type
         if policy_type == OIPolicy:
             policy = policy_type(
                 n_actions=num_actions,
@@ -35,16 +37,21 @@ def run_repetitions(
             )
         else:
             policy = policy_type(n_actions=num_actions)
+        # Run the single repetition of the experiment
         for timestep in range(1, num_timesteps):
+            # Sample next action
             if policy_type == EgreedyPolicy:
                 action_number = policy.select_action(epsilon=kwargs["epsilon"])
             elif policy_type == UCBPolicy:
                 action_number = policy.select_action(c=kwargs["c"], t=timestep)
             else:
                 action_number = policy.select_action()
+            # Sample reward
             reward = bandit.act(a=action_number)
             cum_reward += reward
+            # Update policy
             policy.update(action_number=action_number, reward=reward)
+            # Store average reward
             average_reward[timestep] = cum_reward / timestep
         total_reward += average_reward
 
@@ -60,11 +67,13 @@ def experiment(
         initial_values: list,
         c_values: list
 ) -> None:
-    # To Do: Write all your experiment code here
+    
 
     # Assignment 1: e-greedy
+
     egreedy_graph = LearningCurvePlot(title="E-greedy learning curves based on epsilon hyperparameter")
     epsilons_end_avg_reward = np.zeros(len(epsilons))
+    # Run the experiment for each epsilon value
     for i in range(len(epsilons)):
         curve = run_repetitions(
             num_actions=n_actions,
@@ -73,13 +82,19 @@ def experiment(
             policy_type=EgreedyPolicy,
             epsilon=epsilons[i]
         )
-        egreedy_graph.add_curve(y=smooth(y=curve, window=smoothing_window), label=f"e={epsilons[i]}")
+        egreedy_graph.add_curve(
+            y=smooth(y=curve, window=smoothing_window),
+            label=f"e={epsilons[i]}"
+        )
         epsilons_end_avg_reward[i] = curve[-1]
     egreedy_graph.save(name="egreedy_epsilons_graph.png")
 
+
     # Assignment 2: Optimistic init
+
     optimistic_graph = LearningCurvePlot(title="OI learn. curves based on init. val. hyperparam. (learn. rate = 0.1)")
     initial_values_end_avg_reward = np.zeros(len(initial_values))
+    # Run the experiment for each initial value
     for i in range(len(initial_values)):
         curve = run_repetitions(
             num_actions=n_actions,
@@ -96,9 +111,12 @@ def experiment(
         initial_values_end_avg_reward[i] = curve[-1]
     optimistic_graph.save(name="optimistic_initials_graph.png")
 
+
     # Assignment 3: UCB
+
     ucb_graph = LearningCurvePlot(title="UCB learning curves based on exploration constant hyperparameter")
     c_values_end_avg_reward = np.zeros(len(c_values))
+    # Run the experiment for each value for c
     for i in range(len(c_values)):
         curve = run_repetitions(
             num_actions=n_actions,
@@ -114,16 +132,22 @@ def experiment(
         c_values_end_avg_reward[i] = curve[-1]
     ucb_graph.save(name="ucb_constants_graph.png")
 
+
     # Assignment 4: Comparison
+
     # Comparison plot
+
     comparison_plot = ComparisonPlot(title="Average end reward based on alg. and hyperparameter value")
+    # Using average rewards found in previous sections
     comparison_plot.add_curve(x=epsilons, y=epsilons_end_avg_reward, label="E-greedy")
     comparison_plot.add_curve(x=initial_values, y=initial_values_end_avg_reward, label="Optimistic Initialization (alpha=0.1)")
     comparison_plot.add_curve(x=c_values, y=c_values_end_avg_reward, label="UCB")
     comparison_plot.save("comparison_graph.png")
 
     # Optimal cases plot
+
     optimal_cases_plot = LearningCurvePlot(title="Learning curves of algs. with optimal hyperparameters")
+    # Find optimal epsilon for e-greedy
     epsilon_optimal = epsilons[np.argmax(epsilons_end_avg_reward)]
     epsilon_optimal_curve = run_repetitions(
         num_actions=n_actions,
@@ -132,10 +156,12 @@ def experiment(
         policy_type=EgreedyPolicy,
         epsilon=epsilon_optimal
     )
+    # Add e-greedy to the plot
     optimal_cases_plot.add_curve(
         y=smooth(y=epsilon_optimal_curve, window=smoothing_window),
         label=f"E-greedy (e={epsilon_optimal})"
     )
+    # Find optimal initial value for OI
     initial_value_optimal = initial_values[np.argmax(initial_values_end_avg_reward)]
     oi_optimal_curve = run_repetitions(
         num_actions=n_actions,
@@ -145,10 +171,12 @@ def experiment(
         initial_value=initial_value_optimal,
         learning_rate=0.1  # fixed for our experiments
     )
+    # Add OI to the plot
     optimal_cases_plot.add_curve(
         y=smooth(y=oi_optimal_curve, window=smoothing_window),
         label=f"Optimistic init. (init_val={initial_value_optimal})"
     )
+    # Find optimal c for UCB
     c_optimal = c_values[np.argmax(c_values_end_avg_reward)]
     ucb_optimal_curve = run_repetitions(
         num_actions=n_actions,
@@ -157,10 +185,12 @@ def experiment(
         policy_type=UCBPolicy,
         c=c_optimal
     )
+    # Add UCB to the plot
     optimal_cases_plot.add_curve(
         y=smooth(y=ucb_optimal_curve, window=smoothing_window),
         label=f"UCB (c={c_optimal})"
     )
+    # Save the plot
     optimal_cases_plot.save(name="optimal_comparison_graph.png")
 
 
--- BanditPolicies.py
+++ BanditPolicies.py
@@ -15,23 +15,23 @@ class EgreedyPolicy:
 
     def __init__(self, n_actions=10):
         self.n_actions = n_actions
-        # TO DO: Add own code
-        self.Q_a = np.zeros(n_actions)
-        self.pi_a = np.zeros(n_actions)
+        # Initial means and counts
+        self.Q_a = np.zeros(n_actions) # non-optimistic
         self.n_a = np.zeros(n_actions)
+        # Array for storing the policy (explicitly)
+        self.pi_a = np.zeros(n_actions)
 
     def select_action(self, epsilon):
-        # TO DO: Add own code
-        max_reward_number = np.argmax(self.Q_a)
+        greedy_action = np.argmax(self.Q_a)
         for action_number in range(len(self.pi_a)):
-            if action_number == max_reward_number:
-                self.pi_a[action_number] = 1 - epsilon
+            if action_number == greedy_action:
+                self.pi_a[action_number] = 1 - epsilon # Exploitation
             else:
-                self.pi_a[action_number] = epsilon / (len(self.Q_a) - 1)
+                self.pi_a[action_number] = epsilon / (len(self.Q_a) - 1) # Exploration
         return np.random.choice(len(self.pi_a), p=self.pi_a)
 
     def update(self, action_number, reward):
-        # TO DO: Add own code
+        # Incremental update rule
         self.n_a[action_number] += 1
         self.Q_a[action_number] = self.Q_a[action_number] + (1 / self.n_a[action_number]) * (reward - self.Q_a[action_number])
 
@@ -40,16 +40,17 @@ class OIPolicy:
 
     def __init__(self, n_actions=10, initial_value=0.0, learning_rate=0.1):
         self.n_actions = n_actions
-        # TO DO: Add own code
+        # Initial means
         self.Q_a = np.full(shape=n_actions, fill_value=initial_value)
+        # Learning rate for update rule
         self.learning_rate = learning_rate
 
     def select_action(self):
-        # TO DO: Add own code
+        # Greedy policy
         return np.argmax(self.Q_a)
 
     def update(self, action_number, reward):
-        # TO DO: Add own code
+        # Learning-based update rule
         self.Q_a[action_number] = self.Q_a[action_number] + self.learning_rate*(reward - self.Q_a[action_number])
 
 
@@ -57,18 +58,19 @@ class UCBPolicy:
 
     def __init__(self, n_actions=10):
         self.n_actions = n_actions
-        # TO DO: Add own code
-        self.Q_a = np.zeros(n_actions)
-        self.pi_a = np.zeros(n_actions)
+        # Initial means and counts
+        self.Q_a = np.zeros(n_actions) # non-optimistic
         self.n_a = np.zeros(n_actions)
+        # Array for storing the policy (explicitly)
+        self.pi_a = np.zeros(n_actions)
 
     def select_action(self, c, t):
-        # TO DO: Add own code
+        # UCB policy (the implementation treats the estimate as infinity when self.n_a value is 0, regardless runtime error):
         return np.argmax(self.Q_a + c * np.sqrt((np.log(t)) / self.n_a))
 
     def update(self, action_number, reward):
-        # TO DO: Add own code
         self.n_a[action_number] += 1
+        # Incremental update rule
         self.Q_a[action_number] = self.Q_a[action_number] + (1 / self.n_a[action_number]) * (reward - self.Q_a[action_number])
 
 
